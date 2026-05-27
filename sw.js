03819119ef536eec0edb5029b928adcd375a9015
Sergio <Sergio@MacBook-Air-de-Andrea.local>
2026-05-27 11:39:20 +0200

    Update app 2026-05-27 11:39

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SRC Training – Service Worker
 // Estrategia: Cache First para assets estáticos, Network First para API
 
-const CACHE_NAME = 'src-training-v8';
+const CACHE_NAME = 'src-training-v9';
 const STATIC_ASSETS = [
   '/src-training/',
   '/src-training/index.html',
