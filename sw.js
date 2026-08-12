e2e1e15d8e03106fb4e367b3f00d9fadb14a6ebf
Sergio <Sergio@MacBook-Air-de-Andrea.local>
2026-08-12 08:10:20 +0200

    Add Cereales to Sergio's Método SRC food bank (hidratos principales)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SRC Training – Service Worker
 // Estrategia: Cache First para assets estáticos, Network First para API
 
-const CACHE_NAME = 'src-training-v56';
+const CACHE_NAME = 'src-training-v57';
 const STATIC_ASSETS = [
   '/src-training/',
   '/src-training/index.html',
